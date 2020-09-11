247deb7529715de54e0048ef045fc028ad8f5ee6
Kartik Upadhyay <kartikupadhyay90@gmail.com>
2020-09-11 14:14:47 +0530

    Fix: Add dynamic matcher for the postion

--- robot_spec.rb
+++ robot_spec.rb
@@ -4,6 +4,9 @@ require './robot'
 RSpec.describe Robot do
 
   let (:robot) { described_class.new(5) }
+  let (:direction_x) { 2 }
+  let (:direction_y) { 1 }
+  let (:facing) { Robot::DIRECTIONS.first }
 
   before { allow($stdout).to receive(:write) }
   context 'when robot is not placed' do
@@ -24,38 +27,47 @@ RSpec.describe Robot do
 
   context 'when robot is placed facing valid direction (Non-edge)' do
     before(:each) do
-      robot.place('0, 0, east')
+      robot.place("#{direction_x}, #{direction_y}, #{facing}")
     end
 
     describe '#move' do
       it 'should move one block' do
-        expect { robot.move }.to output("Robot currently placed at 0, 1 facing east direction\n").to_stdout
+        validate_position_and_direction('move', direction_x - 1, direction_y, facing)
       end
     end
 
     describe '#left' do
       it 'should turn left on same position' do
-        expect { robot.left }.to output("Robot currently placed at 0, 0 facing south direction\n").to_stdout
+        validate_position_and_direction('left', direction_x, direction_y, 'west')
       end
     end
 
     describe '#right' do
       it 'should turn right on same position' do
-        expect { robot.right }.to output("Robot currently placed at 0, 0 facing north direction\n").to_stdout
+        validate_position_and_direction('right', direction_x, direction_y, 'east')
       end
     end
 
     describe '#report' do
       it 'should report without any movement' do
-        expect { robot.report }.to output("Robot currently placed at 0, 0 facing east direction\n").to_stdout
+        validate_position_and_direction('report', direction_x, direction_y, facing)
       end
     end
 
     describe '#place' do
       it 'should place the robot' do
-        expect { robot.place('0, 4, west') }.to output("Robot currently placed at 0, 4 facing west direction\n").to_stdout
+        direction_x = 1
+        direction_y = 2
+        facing = 'north'
+        validate_position_and_direction("place", direction_x, direction_y, facing, "#{direction_x}, #{direction_y}, #{facing}")
       end
     end
+  end
+
+  def validate_position_and_direction(command, direction_x, direction_y, facing, args = '')
+    expect {
+      command == 'place' ? robot.send(command, args) : robot.send(command)
+    }.to output("Robot currently placed at #{direction_x}, #{direction_y} facing #{facing} direction\n").to_stdout
   end
 
 end
